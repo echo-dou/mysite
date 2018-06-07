2b6a741d6a29b3e75c596e5cb15a9a9cc82554a3
echo-dou <huangpingsan@163.com>
2018-06-07 18:59:23 +0800

    follow django tutorial

---
+++ polls/reply.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+import time
+
+class Msg(object):
+    def __init__(self):
+        pass
+    def send(self):
+        return "success"
+
+class TextMsg(Msg):
+    """
+    回复类型为文本的消息
+    """
+    # 初始化xml变量
+    def __init__(self, toUserName, fromUserName, content):
+        self.__dict = dict()
+        self.__dict['ToUserName'] = toUserName
+        self.__dict['FromUserName'] = fromUserName
+        self.__dict['CreateTime'] = int(time.time())
+        self.__dict['Content'] = content
+
+    # 发送的文本消息的xml内容
+    def send(self):
+        XmlForm = """
+        <xml>
+        <ToUserName><![CDATA[{ToUserName}]]></ToUserName>
+        <FromUserName><![CDATA[{FromUserName}]]></FromUserName>
+        <CreateTime>{CreateTime}</CreateTime>
+        <MsgType><![CDATA[text]]></MsgType>
+        <Content><![CDATA[{Content}]]></Content>
+        </xml>
+        """
+        return XmlForm.format(**self.__dict)
+    
+class ImageMsg(Msg):
+    """
+    回复类型为图片的消息
+    """
+    # 初始化xml变量
+    def __init__(self, toUserName, fromUserName, mediaId):
+        self.__dict = dict()
+        self.__dict['ToUserName'] = toUserName
+        self.__dict['FromUserName'] = fromUserName
+        self.__dict['CreateTime'] = int(time.time())
+        self.__dict['MediaId'] = mediaId
+
+    # 发送的图片消息的xml内容
+    def send(self):
+        XmlForm = """
+        <xml>
+        <ToUserName><![CDATA[{ToUserName}]]></ToUserName>
+        <FromUserName><![CDATA[{FromUserName}]]></FromUserName>
+        <CreateTime>{CreateTime}</CreateTime>
+        <MsgType><![CDATA[image]]></MsgType>
+        <Image>
+        <MediaId><![CDATA[{MediaId}]]></MediaId>
+        </Image>
+        </xml>
+        """
+        return XmlForm.format(**self.__dict)
+
+if __name__ == "__main__":
+    im=ImageMsg('JAFY','ECHO',1)
+    print(im.send())
+    print(im.__dict)
